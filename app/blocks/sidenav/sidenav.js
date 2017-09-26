8c5567b5950e77254557cebcfa765c0d6f023068
Oleg <c1n1kk@gmail.com>
2017-09-26 10:57:11 +0300

    add action for sidenav

--- app/blocks/sidenav/sidenav.js
+++ app/blocks/sidenav/sidenav.js
@@ -1,12 +1,20 @@
 export default () => {
 	const btnOpen = document.querySelector('.sidenav__btn');
+	const btnClose = document.querySelector('.sidenav__close');
+	const overlay = document.querySelector('.sidenav__overlay');
 	const body = document.querySelector('body');
 	const wrap = document.querySelector('.sidenav__body');
-	const activeClass = 'open-side';
+	const activeClass = 'page_open-side';
 
-	const open = e => {
+	const open = () => {
 		body.classList.add(activeClass);
 	};
 
+	const close = () => {
+		body.classList.remove(activeClass);
+	};
+
 	btnOpen.addEventListener('click', open);
+	overlay.addEventListener('click', close);
+	btnClose.addEventListener('click', close);
 };
