6edf7cc713c265612ac9e148b70cdb5163fe6bb1
Oleg <c1n1kk@gmail.com>
2018-01-11 14:20:12 +0300

    remove

--- app/blocks/select/select.js
+++ app/blocks/select/select.js
@@ -6,18 +6,6 @@ export default {
 	init() {
 		$('.select').select2({
 			width : "resolve",
-			dropdownParent: $('#modal .modal__body')
 		});
-
-		$('.select').on('select2:opening', function(e) {
-			console.log('asfs');
-			$.magnificPopup.open({
-				items: {
-					src: '#modal', // can be a HTML string, jQuery object, or CSS selector
-					type: 'inline'
-				}
-			});
-		})
 	}
-
 };
