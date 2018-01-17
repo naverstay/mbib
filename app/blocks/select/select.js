0927df5a4941a6b06bba79988084985b51e7362a
Oleg <c1n1kk@gmail.com>
2018-01-17 21:57:59 +0300

    open select in modal

--- app/blocks/select/select.js
+++ app/blocks/select/select.js
@@ -7,25 +7,45 @@ export default {
 	init() {
 		$('.select').select2({
 			width : "resolve",
-			dropdownParent: $('#modal .modal__body'),
-			closeOnSelect: false
+			dropdownParent: $('#modal .modal__body')
 		});
 
-		$('.select').on('select2:opening', function(e) {
+		$('.select').on('select2:open', function(e) {
 			const modal = $('.modal_mobile-select');
+			const modalTitle = modal.find('.modal__title')
+			const dropdown = modal.find('.select2-dropdown');
+			const placeholder = $(this).attr('data-placeholder');
+
+			modal.find('.select2-container').css({
+				position : 'relative',
+				top : 0,
+				left : 0
+			});
+
+			dropdown.css({
+				position : 'relative',
+				top : 0,
+				left : 0,
+				width : '100%'
+			});
+
+			modalTitle.text(placeholder);
 
 			$.magnificPopup.open({
 				items: {
 					src: modal, // can be a HTML string, jQuery object, or CSS selector
 					type: 'inline'
-				},
-				callbacks : {
-					open: function() {
-						console.log(modal.find('li').length);
-					}
 				}
 			});
-			
+		})
+
+		$('.select').on('select2:close', function(e) {
+			$.magnificPopup.close({
+				items: {
+					src: modal, // can be a HTML string, jQuery object, or CSS selector
+					type: 'inline'
+				}
+			});
 		})
 	}
 
