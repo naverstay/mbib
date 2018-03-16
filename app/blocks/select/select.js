ee5b975a1becf22791496281806d0ea16e4bdbe0
Oleg <c1n1kk@gmail.com>
2018-03-16 21:49:02 +0300

    add select2 for choose place modal

--- app/blocks/select/select.js
+++ app/blocks/select/select.js
@@ -8,58 +8,69 @@ export default {
 		const isMobile = checkIsMobile();
 		if (!isMobile) {
 			$('.select').select2({
-				width : 'resolve'
+				width : '100%'
 			});
 		} else {
-			$('.select').select2({
-				width : 'resolve',
-				dropdownParent: $('#modal .modal__body')
-			});
+			var selects = $('.select');
 
-			$('.select').on('select2:open', function() {
-				const modal = $('.modal_mobile-select');
-				const modalTitle = modal.find('.modal__title');
-				const dropdown = modal.find('.select2-dropdown');
-				const placeholder = $(this).attr('data-placeholder');
-
-				modal.find('.select2-container').css({
-					'position' : 'relative',
-					'top' : 0,
-					'left' : 0
-				});
-
-				dropdown.css({
-					'position' : 'relative',
-					'top' : 0,
-					'left' : 0,
-					'width' : '100%'
-				});
-
-				modalTitle.text(placeholder);
-
-				setTimeout(() => {
-					$.magnificPopup.open({
-						alignTop : true,
-						fixedContentPos : true,
-						items: {
-							src: modal, // can be a HTML string, jQuery object, or CSS selector
-							type: 'inline'
-						}
+			for (const select of selects) {
+				if ($(select).hasClass('select_dont-use-modal')) {
+					$(select).select2({
+						width : '100%',
+						dropdownParent: $('#modalPlace .modal__body')
 					});
-				}, 0)
+				} else {
 
-				
-			});
+					$(select).select2({
+						width : '100%',
+						dropdownParent: $('#modal .modal__body')
+					});
 
-			$('.select').on('select2:close', function() {
-				const modal = $('.modal_mobile-select');
-				$.magnificPopup.close({
-					items: {
-						src: modal, // can be a HTML string, jQuery object, or CSS selector
-						type: 'inline'
-					}
-				});
-			});
+					$(select).on('select2:open', function() {
+						const modal = $('.modal_mobile-select');
+						const modalTitle = modal.find('.modal__title');
+						const dropdown = modal.find('.select2-dropdown');
+						const placeholder = $(this).attr('data-placeholder');
+
+						modal.find('.select2-container').css({
+							'position' : 'relative',
+							'top' : 0,
+							'left' : 0
+						});
+
+						dropdown.css({
+							'position' : 'relative',
+							'top' : 0,
+							'left' : 0,
+							'width' : '100%'
+						});
+
+						modalTitle.text(placeholder);
+
+						setTimeout(() => {
+							$.magnificPopup.open({
+								alignTop : true,
+								fixedContentPos : true,
+								items: {
+									src: modal, // can be a HTML string, jQuery object, or CSS selector
+									type: 'inline'
+								}
+							});
+						}, 0)
+					});
+
+					$(select).on('select2:close', function() {
+						const modal = $('.modal_mobile-select');
+						$.magnificPopup.close({
+							items: {
+								src: modal, // can be a HTML string, jQuery object, or CSS selector
+								type: 'inline'
+							}
+						});
+					});
+				}
+			}
+
 		}
 	}
 }
