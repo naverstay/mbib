9434c24aabf0c578f543ccceb24d56071382474c
Oleg <c1n1kk@gmail.com>
2018-05-29 16:02:55 +0300

    upd search

--- app/blocks/search/search.js
+++ app/blocks/search/search.js
@@ -1,17 +1,25 @@
 const search = (() => {
 	const wrap = document.querySelector('.search');
-	
+
 	if (!wrap) return;
 
 	const switcher = wrap.querySelector('.search__switcher');
 	const switcherText = switcher.querySelector('.btn__text');
 	const switcherLabelOpen = "Скрыть поиск";
 	const switcherLabelСlose = "Расширенный поиск";
-	// const advanced = wrap.querySelector('.search__advanced');
+	const advanced = wrap.querySelector('.search__advanced');
 
 	switcher.addEventListener('click', () => {
-		wrap.classList.contains('search_show-advanced') ? switcherText.textContent = switcherLabelСlose : switcherText.textContent = switcherLabelOpen;
+		switcherText.textContent = switcherLabelOpen;
+		if (wrap.classList.contains('search_show-advanced')) {
+			switcherText.textContent = switcherLabelСlose
+			advanced.style.overflow = 'hidden';
+		}
 		wrap.classList.toggle('search_show-advanced');
+	})
+
+	wrap.addEventListener('transitionend', () => {
+		wrap.classList.contains('search_show-advanced') ? advanced.style.overflow = 'visible' : '';
 	})
 
 	const switcherGroup = document.querySelectorAll('.search .form__group-name');
@@ -22,10 +30,7 @@ const search = (() => {
 			const wrap = event.target.closest('.form__group_is-accordion');
 			wrap.classList.toggle('form__group_is-accordion-open');
 		})
-		
 	}
-
-
 })();
 
 export default search
