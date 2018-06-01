f323e0c02b96eae2875c620fcfa28e7a633e8edc
Oleg <c1n1kk@gmail.com>
2018-06-01 09:19:32 +0300

    upd phone mask

--- app/blocks/phone-field/phoneField.js
+++ app/blocks/phone-field/phoneField.js
@@ -1,19 +1,18 @@
 import $ from 'jquery';
 import IMask from 'imask';
-import intlTelInput from './intlTelInput.js';
 
 const phoneField = (() => {
-	$('.phone-field').intlTelInput({
-		initialCountry : 'ru'
-	});
 
-	const phone = document.querySelector('.phone-field');
+	const phones= document.querySelectorAll('.phone-field');
 
-	if (phone) {
-		const maskOptions = {
-			mask: '000 000 00 00'
-		};
-		const mask = new IMask(phone, maskOptions);
+	if (phones) {
+		phones.forEach(phone => {
+			const maskOptions = {
+				mask: '+7 000 000 00 00'
+			};
+			
+			const mask = new IMask(phone, maskOptions);
+		})
 	}
 })();
 
