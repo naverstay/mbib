bdb72c7a7a867424adac0cfa9e9730f5391429dc
Oleg Denisov <c1n1kk@gmail.com>
2019-07-30 20:50:39 +0300

    upgallery

--- app/blocks/thumb-gallery/thumb-gallery.js
+++ app/blocks/thumb-gallery/thumb-gallery.js
@@ -6,17 +6,38 @@ const thumbGallery = (() => {
 
   const isMobile = checkIsMobile();
 
-  $('.thumb-gallery').slick({
+  $('.thumb-gallery__inner').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     speed: 0,
-    lazyLoad: 'progressive',
+    lazyLoad: 'ondemand',
     dots: true
   });
 
+  const loader = '<div class="loader loader_active thumb-gallery__loader"><div class="loader__overlay"><div class="loader__spiner"></div></div></div>';
+
+  $('.thumb-gallery__inner').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    const wrap = $(event.target).parent();
+    const notLoadedImage = wrap.find('slick-loading');
+    if (notLoadedImage.length === 0) {
+      wrap.append(loader);
+    }
+  });
+
+  $('.thumb-gallery__inner').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+    const wrap = $(event.target).parent();
+    const loaderThumb = wrap.find('.loader');
+    const notLoadedImage = $(currentSlide).find('img[data-lazy]');
+    console.log(notLoadedImage);
+    if (notLoadedImage.length === 0) {
+      loaderThumb.remove();
+    }
+
+  });
+
   if (!isMobile) {
-    $('.thumb-gallery .slick-dots button').mouseenter((e) => {
+    $('.thumb-gallery__inner .slick-dots:not(.slick-active) button').mouseenter((e) => {
       $(e.target).trigger('click');
     });
   }
