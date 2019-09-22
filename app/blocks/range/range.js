44eefe0521ed3342ff2ac620a546d8471800b00f
Oleg Denisov <c1n1kk@gmail.com>
2019-09-22 19:44:36 +0300

    add rules

--- app/blocks/range/range.js
+++ app/blocks/range/range.js
@@ -2,28 +2,43 @@ import $ from 'jquery';
 import rangeSlider from 'rangeslider-pure';
 
 const range = (() => {
-	const slider = document.querySelectorAll('input[type="range"]');
-	if (slider.length) {
-		rangeSlider.create(slider, {
-			polyfill: true
-		});
-		
-		const powerRange = document.querySelector('.range_power');
-		const powerRangeOutput = document.querySelector('.form__output_power');
+  const slider = document.querySelectorAll('input[type="range"]');
+  if (slider.length) {
+    rangeSlider.create(slider, {
+      polyfill: true
+    });
 
-		powerRange.addEventListener('input', (event) => {
-			powerRangeOutput.innerHTML = powerRange.value;
-		})
+    const powerRange = document.querySelector('.range_power');
+    const powerRangeOutput = document.querySelector('.form__output_power');
 
+    if (powerRange) {
+      powerRange.addEventListener('input', (event) => {
+        powerRangeOutput.innerHTML = powerRange.value;
+      });
+    }
 
-		const valueRange = document.querySelector('.range_value');
-		const valueRangeOutput = document.querySelector('.form__output_value');
+    const valueRange = document.querySelector('.range_value');
+    const valueRangeOutput = document.querySelector('.form__output_value');
 
-		valueRange.addEventListener('input', (event) => {
-			valueRangeOutput.innerHTML = valueRange.value;
-		})
-	}
+    if (valueRange) {
+      valueRange.addEventListener('input', (event) => {
+        valueRangeOutput.innerHTML = valueRange.value;
+      });
+    }
 
+  }
+
+  const radiusRange = document.querySelectorAll('.geoplace__radius');
+
+  if (radiusRange.length) {
+    rangeSlider.create(radiusRange, {
+      polyfill: true,
+      step: 1,
+      min: 0,
+      max: 6,
+      value: 0
+    });
+  }
 })();
 
-export default range
+export default range;
