0f121463bfa73c0dee6bd5df624a63b13d13e2eb
Oleg Denisov <c1n1kk@gmail.com>
2021-02-28 15:07:25 +0300

    добавить страницу кода

--- app/blocks/input/input.js
+++ app/blocks/input/input.js
@@ -1,59 +1,70 @@
+import IMask from 'imask';
+
 const digitpretify = () => {
-	const inputs = document.querySelectorAll('.input_digit-pretify');
-	let oldValue;
-	let oldCursor;
-	const regex = new RegExp(/^\d{0,15}$/g);
-	const mask = function (value) {
-		const newValue = value.split("").reverse().join("").replace(/\d{3}(?!$|(?:\s$))/g, "$& ").split("").reverse().join("");
-		return newValue;
-	};
+  const inputs = document.querySelectorAll('.input_digit-pretify');
+  let oldValue;
+  let oldCursor;
+  const regex = new RegExp(/^\d{0,15}$/g);
+  const mask = function (value) {
+    const newValue = value.split('').reverse().join('').replace(/\d{3}(?!$|(?:\s$))/g, '$& ').split('').reverse().join('');
+    return newValue;
+  };
 
-	const unmask = function (value) {
-		const output = value.replace(new RegExp(/[^\d]/, 'g'), ''); // Remove every non-digit character
-		return output;
-	};
+  const unmask = function (value) {
+    const output = value.replace(new RegExp(/[^\d]/, 'g'), ''); // Remove every non-digit character
+    return output;
+  };
 
-	const checkSeparator = function (position, interval) {
-		return Math.floor(position / (interval + 1));
-	};
+  const checkSeparator = function (position, interval) {
+    return Math.floor(position / (interval + 1));
+  };
 
-	const keydownHandler = function (e) {
-		const el = e.target;
+  const keydownHandler = function (e) {
+    const el = e.target;
 
-		oldValue = el.value;
-		oldCursor = el.selectionEnd;
-	};
+    oldValue = el.value;
+    oldCursor = el.selectionEnd;
+  };
 
-	const inputHandler = function (e) {
-		const el = e.target;
-		let newCursorPosition;
-		let newValue = unmask(el.value);
+  const inputHandler = function (e) {
+    const el = e.target;
+    let newCursorPosition;
+    let newValue = unmask(el.value);
 
-		if ( newValue.match(regex) ) {
-			newValue = mask(newValue);
+    if ( newValue.match(regex) ) {
+      newValue = mask(newValue);
 
-			newCursorPosition = oldCursor - checkSeparator(oldCursor, 3) + checkSeparator(oldCursor + (newValue.length - oldValue.length), 3) + (unmask(newValue).length - unmask(oldValue).length);
+      newCursorPosition = oldCursor - checkSeparator(oldCursor, 3) + checkSeparator(oldCursor + (newValue.length - oldValue.length), 3) + (unmask(newValue).length - unmask(oldValue).length);
 
-			if ( newValue !== '' ) {
-				el.value = newValue;
-			}
-			else
-			{
-				el.value = '';
-			}
-		}
-		else
-		{
-			el.value = oldValue;
-			newCursorPosition = oldCursor;
-		}
-		el.setSelectionRange(newCursorPosition, newCursorPosition);
-	};
+      if ( newValue !== '' ) {
+        el.value = newValue;
+      }else {
+        el.value = '';
+      }
+    }else {
+      el.value = oldValue;
+      newCursorPosition = oldCursor;
+    }
+    el.setSelectionRange(newCursorPosition, newCursorPosition);
+  };
 
-	inputs.forEach( input => {
-		input.addEventListener('keydown', keydownHandler);
-		input.addEventListener('input', inputHandler);
-	});
+  inputs.forEach( input => {
+    input.addEventListener('keydown', keydownHandler);
+    input.addEventListener('input', inputHandler);
+  });
+
+  const inputsNumber = document.querySelectorAll('.code-phone');
+  if (inputsNumber.length > 0) {
+
+    inputsNumber.forEach( input => {
+      const maskOptions = {
+        mask: '[0000]'
+      };
+
+      const mask = new IMask(input, maskOptions);
+    });
+  }
+
 };
 
 export default digitpretify;
--- app/blocks/phone-field/phoneField.js
+++ app/blocks/phone-field/phoneField.js
@@ -3,35 +3,36 @@ import IMask from 'imask';
 
 const phoneField = (() => {
 
-	const phones= document.querySelectorAll('.js-phone-field__control');
+  const phones = document.querySelectorAll('.js-phone-field__control');
 
-	if (phones) {
-		phones.forEach(phone => {
-			const maskOptions = {
-				mask: '+{7} 000 000 00 00'
-			};
-			
-			const mask = new IMask(phone, maskOptions);
-		})
-	}
+  if (phones) {
+    phones.forEach(phone => {
+      const maskOptions = {
+        mask: '+{7} 000 000 00 00'
+      };
 
-	const cloneButton = $('.js-phone-field__clone');
-	const removeButton = $('.js-phone-field__remove');
+      const mask = new IMask(phone, maskOptions);
+    });
+  }
 
-	cloneButton.on('click', (event) => {
-		const phoneRow = $(event.target).closest('.js-row_can-cloned');
-		const clonePhoneRow = phoneRow.clone(true);
-		const newId = phoneRow.find('input').attr('id') + Date.now();
-		clonePhoneRow.find('label').attr('for', newId);
-		clonePhoneRow.find('input').attr('id', newId);
-		phoneRow.after(clonePhoneRow);
-	})
+  const cloneButton = $('.js-phone-field__clone');
+  const removeButton = $('.js-phone-field__remove');
 
-	removeButton.on('click', (event) => {
-		const phoneRow = $(event.target).closest('.js-row_can-cloned');
-		phoneRow.remove();
-	})
+  cloneButton.on('click', (event) => {
+    const phoneRow = $(event.target).closest('.js-row_can-cloned');
+    const clonePhoneRow = phoneRow.clone(true);
+    const newId = phoneRow.find('input').attr('id') + Date.now();
+    clonePhoneRow.find('label').attr('for', newId);
+    clonePhoneRow.find('input').attr('id', newId);
+    phoneRow.after(clonePhoneRow);
+  });
+
+  removeButton.on('click', (event) => {
+    const phoneRow = $(event.target).closest('.js-row_can-cloned');
+    phoneRow.remove();
+  });
 
 })();
 
 export default phoneField
+;
