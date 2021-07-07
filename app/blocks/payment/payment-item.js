937aed57746996f37be1b411d96fb7e428411668
Krainov <krainoff.alesha@yandex.ru>
2021-07-07 09:36:14 +0300

    модалка

--- app/blocks/payment/payment-item.js
+++ app/blocks/payment/payment-item.js
@@ -4,8 +4,13 @@ const paymentItem = (() => {
     const item = $('.js-payment-item')
     const itemClassActive = 'js-payment-item_active'
     item.on('click', function() {
+
         item.removeClass(itemClassActive)
         const el = $(this);
+        const control =  el.attr('data-id')
+        $('.payment-content').hide()
+        $('#' + control).show()
+
         el.addClass(itemClassActive)
     })
 })();
