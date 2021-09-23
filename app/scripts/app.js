280e4f2851902e9e03c46b8a0ba25d72165adb11
nv <naverstay.me@gmail.com>
2021-09-23 08:45:55 +0300

    Roboto

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,26 +29,35 @@ import 'lazysizes';
 import 'notyf';
 
 const checkTouch = () => {
-    if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-        document.body.classList.add('page_touch');
-    }
+  if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+    document.body.classList.add('page_touch');
+  }
 };
 
 $(() => {
-    svg4everybody();
+  svg4everybody();
 
-    checkTouch();
+  checkTouch();
 
-    sidenav();
+  sidenav();
 
-    carousel.init();
+  carousel.init();
 
-    gallery.init();
+  gallery.init();
 
-    collapse();
+  collapse();
 
-    digitpretify();
+  digitpretify();
 
-    $('.tile').on('click', () => { return true; });
+  $('.tile').on('click', () => {
+    return true;
+  });
+
+  $('.toggleError').on('click', function () {
+    let btn = $(this);
+    let input = $('#' + btn.attr('data-error'));
+    input.toggleClass('input_invalid');
+    return false;
+  });
 
 });
