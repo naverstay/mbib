7e37f3c4c7ad01c45f481da3359cb6394fed004d
nv <naverstay.me@gmail.com>
2021-12-04 20:07:11 +0200

    side menu update

--- app/blocks/nav/nav.js
+++ app/blocks/nav/nav.js
@@ -1,13 +1,15 @@
 const nav = (() => {
   const triggers = document.querySelectorAll('.nav_sidenav .nav__trigger');
-    for (let trigger of triggers) {
-      trigger.addEventListener('click', function (event) {
-        const nv = event.currentTarget.closest('.nav__item');
-        const dropdown = nv.querySelector('.nav__dropdown');
-        dropdown.classList.toggle('nav__dropdown_open');
-      });
-    }
+  for (let trigger of triggers) {
+    trigger.addEventListener('click', function (event) {
+      const nv = event.currentTarget.closest('.nav__item');
+      const dropdown = nv.querySelector('.nav__dropdown');
+      nv.classList.toggle('nav__item_open');
+      dropdown.classList.toggle('nav__dropdown_open');
+    });
+  }
 
+  return false;
 })();
 
 export default nav;
